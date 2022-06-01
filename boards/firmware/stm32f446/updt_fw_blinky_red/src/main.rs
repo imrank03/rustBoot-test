83bc0c9008cafefaa6ed7b3a30519f4032641ffe
@imrank03 <immu0396@gmail.com>
2022-06-01 23:35:54 +0530

    first commit

---
+++ boards/firmware/stm32f446/updt_fw_blinky_red/src/main.rs
@@ -0,0 +1,52 @@
+#![no_main]
+#![no_std]
+
+extern crate cortex_m;
+extern crate cortex_m_rt;
+
+extern crate stm32f4xx_hal as mcu;
+
+use cortex_m::peripheral::Peripherals;
+use cortex_m_rt::entry;
+use crate::mcu::{
+    pac,
+    prelude::*,
+};
+use panic_probe as _;
+
+use rustBoot_hal::stm::stm32f446::FlashWriterEraser;
+use rustBoot_update::update::{update_flash::FlashUpdater, UpdateInterface};
+
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+    let gpio = dp.GPIOA.split();
+    let mut led = gpio.pa5.into_push_pull_output();
+
+    let rcc = dp.RCC.constrain();
+    // let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();    
+    let mut count :i32 = 0;
+    
+    // let mut delay = dp.TIM1.delay_ms(&clocks);
+
+    while count < 6 {
+        led.toggle();
+        cortex_m::asm::delay(8000000);
+        count = count + 1;
+    }
+    
+    let flash1 = dp.FLASH;
+
+    let flash_writer = FlashWriterEraser { nvm: flash1 };
+    let updater = FlashUpdater::new(flash_writer);
+    match updater.update_success() {
+        Ok(_v) => {}
+        Err(e) => panic!("couldnt trigger update: {}", e),
+    }
+
+    loop {
+        led.toggle();
+        cortex_m::asm::delay(8000000);
+    }
+}
